e1c340e7a03e7701f139d0fdd753f6b5dae6423d
kysugar <714572000@qq.com>
2016-03-13 18:08:24 +0800

    version ky

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include "ky.h"
 using namespace std;
 
 int main() {
 	cout << "Hello world" << endl;
 	cout << "version 2" << endl;
 	cout << "version 3" << endl;
+	print_ky();
 }
