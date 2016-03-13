8120041ed8b331b260b9c13f808069c89c74030d
kysugar <714572000@qq.com>
2016-03-13 18:04:14 +0800

    tlm version1

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
-
+#include "tlm.h"
 using namespace std;
 
 int main() {
 	cout << "Hello world" << endl;
 	cout << "version 2" << endl;
 	cout << "version 3" << endl;
+	print_tlm();
 }
---
+++ tlm.cpp
@@ -0,0 +1,6 @@
+#include "tlm.h"
+#include <iostream>
+using namespace std;
+int print_tlm() {
+	cout << "Hello I am Tlm" << endl;
+}
